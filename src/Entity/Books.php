a55f7034b3f8a3d81af98003ee2b252a81f295dc
Hinamoure <julia.debray@gmail.com>
2021-10-06 11:15:07 +0200

    FEATURE: add a book to the catalog

---
+++ src/Entity/Books.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BooksRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BooksRepository::class)
+ */
+class Books
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $literaryGenre;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $releaseDate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getLiteraryGenre(): ?string
+    {
+        return $this->literaryGenre;
+    }
+
+    public function setLiteraryGenre(string $literaryGenre): self
+    {
+        $this->literaryGenre = $literaryGenre;
+
+        return $this;
+    }
+
+    public function getReleaseDate(): ?\DateTimeInterface
+    {
+        return $this->releaseDate;
+    }
+
+    public function setReleaseDate(?\DateTimeInterface $releaseDate): self
+    {
+        $this->releaseDate = $releaseDate;
+
+        return $this;
+    }
+}
---
+++ src/Form/BooksType.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Books;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class BooksType extends AbstractType
+{
+    public $choices = [
+        'Polar'=>'Polar',
+        'Fantastique'=>'Fantastique',
+        'Science-fiction'=>'Science-fiction'
+    ];
+
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('author', TextType::class,
+                [
+                'required'=>false,
+                'label'=>'Auteur'])
+            ->add('title',TextType::class, [
+                'required'=>false,
+                'label'=>'Titre'])
+            ->add('image', FileType::class, [
+                'required'=>false,
+                'label'=>'Image de couverture',
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpg',
+                            'image/jpeg',
+                        ],
+                        'mimeTypesMessage' => 'Seul les formats JPEG et JPG sont acceptés',
+                    ])
+                ],
+                ])
+            ->add('description', TextareaType::class, [
+                'required'=>false,
+                'label'=>'Description',
+            ])
+            ->add('literaryGenre', ChoiceType::class,
+                [
+                'required'=>false,
+                'placeholder'=>'Dérouler la liste',
+                'label'=>'Genre du livre',
+                    'choices'=> $this->choices,
+                ])
+            ->add('releaseDate', DateType::class, [
+                'required'=>false,
+                'label'=>'Date de parution',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Books::class,
+        ]);
+    }
+}
