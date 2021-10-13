ab3ef0f020bdf44eeec8b2274aa26d45b33e7833
Hinamoure <julia.debray@gmail.com>
2021-10-13 15:31:03 +0200

    FIX: remove final from class to deploy

--- src/Entity/Administrators.php
+++ src/Entity/Administrators.php
@@ -4,12 +4,11 @@ namespace App\Entity;
 
 use App\Repository\AdministratorsRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\User;
 
 /**
  * @ORM\Entity(repositoryClass=AdministratorsRepository::class)
  */
-final class Administrators extends Users
+class Administrators extends Users
 {
     /**
      * @ORM\Id
--- src/Entity/Employees.php
+++ src/Entity/Employees.php
@@ -4,12 +4,11 @@ namespace App\Entity;
 
 use App\Repository\EmployeesRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=EmployeesRepository::class)
  */
-final class Employees extends Users
+class Employees extends Users
 {
     /**
      * @ORM\Id
