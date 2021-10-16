de9bbc1b6ccfdcd0e3e0c3c731c86c797e2872a5
Hinamoure <julia.debray@gmail.com>
2021-10-16 17:09:49 +0200

    DATA: add an admin to database

--- migrations/Version20211016145511.php
+++ migrations/Version20211016145511.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211011080018 extends AbstractMigration
+final class Version20211016145511 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20211011080018 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE books DROP status');
+        $this->addSql('INSERT INTO users(id, email, roles, password, status, type) VALUES ("56", "admin@admin.com", "[\"ROLE_ADMIN\"]", "$2y$13$6isO0d6TewmVC01CgmvM5uCKF9XXTRaWfU5SF9VFwJjxzBsP2V0zK", "validated", "administrator")');
+        $this->addSql('INSERT INTO administrators(id) VALUES ("56")');
     }
 
     public function down(Schema $schema): void
+
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE books ADD status VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+
     }
 }
